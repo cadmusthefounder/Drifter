c8352425330d364b83a706658e1877ef7e3d7f33
Charlton Lim <cadmusthefounder@gmail.com>
2019-02-19 16:20:47 +0800

    fix bug

--- model.py
+++ model.py
@@ -39,13 +39,13 @@ class Model:
             'metric': 'auc' 
         }
         self._search_space = {
-            'n_estimators': hp.quniform('n_estimators', 200, 500, 50), 
-            'max_depth': hp.quniform('max_depth', 5, 7, 1), 
-            'num_leaves': hp.quniform('num_leaves', 30, 90, 4), 
-            'max_bin': hp.quniform('max_bin', 60, 100, 5),
+            'n_estimators': int(hp.quniform('n_estimators', 200, 500, 50)), 
+            'max_depth': int(hp.quniform('max_depth', 5, 7, 1)), 
+            'num_leaves': int(hp.quniform('num_leaves', 30, 90, 4)), 
+            'max_bin': int(hp.quniform('max_bin', 60, 100, 5)),
             'feature_fraction': hp.loguniform('feature_fraction', 0.2, 0.8), 
             'bagging_fraction': hp.loguniform('bagging_fraction', 0.2, 0.8), 
-            'bagging_freq': hp.quniform('bagging_freq', 2, 5, 1), 
+            'bagging_freq': int(hp.quniform('bagging_freq', 2, 5, 1)), 
             'boosting_type': 'gbdt', 
             'objective': 'binary',
             'metric': 'auc'
