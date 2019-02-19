b3385f5eb79eedd18cc2546f643aa897707e4b3a
Charlton Lim <cadmusthefounder@gmail.com>
2019-02-19 16:15:55 +0800

    add tuner

---
+++ hyperparameters_tuner.py
@@ -0,0 +1,53 @@
+import numpy as np
+from sklearn.metrics import roc_auc_score
+from hyperopt import fmin, tpe, space_eval, STATUS_OK, Trials
+
+class HyperparametersTuner:
+
+    def __init__(self, classifier_class, fixed_hyperparameters, search_space, max_evaluations=50):
+        self._classifier_class = classifier_class
+        self._fixed_hyperparameters = fixed_hyperparameters
+        self._search_space = search_space
+        self._max_evaluations = max_evaluations
+
+    def get_best_hyperparameters(self, data, labels):
+        self._training_data = data
+        self._training_labels = labels 
+
+        # Try fixed hyperparameters
+        classifier = self._classifier_class()
+        classifier.set_params(**self._fixed_hyperparameters)
+        predictions = self._classifier.predict(data)
+
+        print('\nFixed hyperparameters')
+        print('self._training_labels.shape: {}'.format(self._training_labels.shape))
+        print('predictions.shape: {}\n'.format(predictions.shape))
+        
+        fixed_hyperparameters_score = roc_auc_score(self._training_labels, predictions)
+
+        # Find best trial hyperparameters
+        trials = Trials()
+        best = fmin(
+            fn=self.objective,
+            space=self._search_space,
+            algo=tpe.suggest,
+            trials=trials,
+            max_evals=self._max_evaluations
+        )
+        best_trial_hyperparameters = space_eval(self._search_space, best)
+        best_trial_hyperparameters_score = 1 - np.min([x['loss'] for x in trials.results])
+
+        return self._fixed_hyperparameters if fixed_hyperparameters_score > best_trial_hyperparameters_score else best_trial_hyperparameters
+
+    def objective(self, trial_hyperparameters):
+        classifier = self._classifier_class()
+        classifier.set_params(**trial_hyperparameters)
+        classifier.fit(self._training_data, self._training_labels)
+        predictions = self._classifier.predict(data)
+
+        print('\nTrial hyperparameters')
+        print('self._training_labels.shape: {}'.format(self._training_labels.shape))
+        print('predictions.shape: {}\n'.format(predictions.shape))
+        
+        trial_score = roc_auc_score(self._training_labels, predictions)
+        return {'loss': (1 - trial_score), 'status': STATUS_OK }
--- model.py
+++ model.py
@@ -10,36 +10,55 @@ pip_install('hyperopt')
 pip_install('lightgbm')
 
 from lightgbm import LGBMClassifier
+from hyperopt import hp
+from hyperparameters_tuner import HyperparametersTuner
 
 class Model:
 
     def __init__(self, datainfo, timeinfo):
         info = self._extract(datainfo, timeinfo)
+        self._print_data_info(info)
+        self._print_time_info(info)
         
         self._max_training_data = 200000
         self._training_data = np.array([])
         self._training_labels = np.array([])
-        self._clf = LGBMClassifier()
-        self._hyperparameters = {
+        self._classifier = None
+        self._classifier_class = LGBMClassifier
+        self._fixed_hyperparameters = {
             'learning_rate': 0.01, 
             'n_estimators': 400, 
-            'max_depth': 12, 
+            'max_depth': 7, 
             'num_leaves': 50, 
-            'max_bin': 150,
+            'max_bin': 63,
             'feature_fraction': 0.6, 
             'bagging_fraction': 0.6, 
-            'bagging_freq': 2, 
+            'bagging_freq': 3, 
             'boosting_type': 'gbdt', 
             'objective': 'binary', 
             'metric': 'auc' 
         }
+        self._search_space = {
+            'n_estimators': hp.quniform('n_estimators', 200, 500, 50), 
+            'max_depth': hp.quniform('max_depth', 5, 7, 1), 
+            'num_leaves': hp.quniform('num_leaves', 30, 90, 4), 
+            'max_bin': hp.quniform('max_bin', 60, 100, 5),
+            'feature_fraction': hp.loguniform('feature_fraction', 0.2, 0.8), 
+            'bagging_fraction': hp.loguniform('bagging_fraction', 0.2, 0.8), 
+            'bagging_freq': hp.quniform('bagging_freq', 2, 5, 1), 
+            'boosting_type': 'gbdt', 
+            'objective': 'binary',
+            'metric': 'auc'
+        }
+        self._best_hyperparameters = None
         
     def fit(self, F, y, datainfo, timeinfo):
-        print('Entering fit function')
+        print('\nEntering fit function')
 
         info = self._extract(datainfo, timeinfo)
+        self._print_time_info(info)
+        
         data = self._fill_nan(F, info)
-
         print('data.shape: {}'.format(data.shape))
         print('y.shape: {}'.format(y.shape))
 
@@ -50,23 +69,30 @@ class Model:
         print('self._training_labels.shape: {}'.format(self._training_labels.shape))
 
         if self._too_much_training_data():
-            print('Removing excess data')
+            print('\nRemoving excess data')
             self._remove_excess_training_data()
             print('self._training_data.shape: {}'.format(self._training_data.shape))
-            print('self._training_labels.shape: {}'.format(self._training_labels.shape))
+            print('self._training_labels.shape: {}\n'.format(self._training_labels.shape))
 
-        self._clf.set_params(**self._hyperparameters)
-        self._clf.fit(self._training_data, self._training_labels)
+        if self._classifier is None or self._best_hyperparameters is None:
+            tuner = HyperparametersTuner(self._classifier_class, self._fixed_hyperparameters, self._search_space)
+            self._best_hyperparameters = tuner.get_best_hyperparameters(self._training_data, self._training_labels)
+            self._classifier = self._classifier_class()
+            self._classifier.set_params(**self._best_hyperparameters)
+        
+        self._classifier.fit(self._training_data, self._training_labels)
 
     def predict(self, F, datainfo, timeinfo):
-        print('Entering predict function')
+        print('\nEntering predict function')
 
         info = self._extract(datainfo, timeinfo)
+        self._print_time_info(info)
+
         data = self._fill_nan(F, info)
         print('data.shape: {}'.format(data.shape))
 
-        probabilities = self._clf.predict_proba(data)[:,1]
-        print('probabilities.shape: {}'.format(probabilities.shape))
+        probabilities = self._classifier.predict_proba(data)[:,1]
+        print('probabilities.shape: {}\n'.format(probabilities.shape))
         return probabilities
   
     def save(self, path="./"):
@@ -124,6 +150,17 @@ class Model:
             'overall_time_spent': overall_time_spent,
             'dataset_time_spent': dataset_time_spent
         }
+
+    def _print_data_info(self, info):
+        print('\nDataset budget: {0:d} seconds'.format(info['time_budget']))
+        print('No. of time features: {0:d}'.format(info['no_of_time_features']))
+        print('No. of numerical features: {0:d}'.format(info['no_of_numerical_features']))
+        print('No. of categorical features: {0:d}'.format(info['no_of_categorical_features']))
+        print('No. of mvc features: {0:d}\n'.format(info['no_of_mvc_features']))
+
+    def _print_time_info(self, info):
+        print('\nOverall time spent: {0:5.2f} seconds'.format(info['overall_time_spent']))
+        print('Dataset time spent: {0:5.2f} seconds\n'.format(info['dataset_time_spent'])) 
 
     def _fill_nan(self, F, info):
         
