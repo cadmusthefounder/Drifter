504194ec0846fb51af18c08626702e90c584d58d
Charlton Lim <cadmusthefounder@gmail.com>
2019-02-21 02:02:41 +0800

    refactor code

--- architectures.py
+++ architectures.py
@@ -131,6 +131,6 @@ class BiasedReservoirSampler_LightGBM:
             transformed_data = np.concatenate((transformed_data, encoded_mvc_data), axis=1)
 
         print('transformed_data.shape: {}'.format(transformed_data.shape))
-        probabilities = self._classifier.predict_proba(transformed_data)[:,1]
+        probabilities = self._classifier.predict_proba(transformed_data)
         print('probabilities.shape: {}\n'.format(probabilities.shape))
         return probabilities
